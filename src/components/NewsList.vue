525389d2b68ea594af23d21fc3b63475c290aa7f
Jamaal Henry <jamaalhenry30@gmail.com>
2022-04-04 16:10:41 -0500

    added news article as cards using css and html as well as completed functionality of search method

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,15 +1,83 @@
 <template> 
-    <ul class="news__list">
-        <li class="news__item">News item 1</li>       
-        <li class="news__item">News item 2</li>       
-        <li class="news__item">News item 3</li>
-    </ul> 
+    <form @submit.prevent="searchNews" class="d-flex flex-column justify-content-center">
+        <div class="input-group mx-sm-3 mb-2">
+            <label class="visually-hidden" for="search">Search</label>
+            <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+            <button class="btn btn-primary mb-2">Search</button>
+        </div>
+         <p>You are searching for {{ searchTerm }}</p>     
+    </form>
+
+
+
+
+    <div class="cont">
+            <div v-for="article in articles" class="card">
+                <img class = "card-image-top" :src="article.urlToImage" width=300 height=200>
+                    <div class="card-body">
+
+                            <h5 class="card-title">{{ article.title }}</h5>
+                            <p class="card-text">{{article.description}}</p>
+                      
+                    </div>
+            </div> 
+    </div>
 </template> 
 
 <script> 
     export default {   
         data() {     
-            return {};   
+            return {
+                articles: [],
+                searchTerm: ''
+            }   
         }, 
-    }; 
+        created(){
+            let self = this;
+            fetch('https://newsapi.org/v2/top-headlines?country=us',
+        {
+            headers: {
+                'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`
+            }
+        })
+            .then(function(response) {
+                return response.json(); 
+            })
+            .then(function(data) {
+                console.log(data);
+                self.articles = data.articles;
+             }); 
+        },
+        methods:{
+            searchNews(){
+            let self = this;
+            fetch('https://newsapi.org/v2/everything?q='+ self.searchTerm + '&language=en',{
+            headers:{
+                'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`,
+            }
+        })
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                console.log(data);
+                self.articles = data.articles;
+             });
+        }
+    }
+}
+        
+     
 </script>
+
+<style>
+.card{
+    width: 300px;
+    margin-top: 20px;
+}
+.cont{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+</style>
